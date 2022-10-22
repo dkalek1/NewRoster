aa506c282add1b39311ecbc67a8d51dcb8a35bb6
dkalek1 <103354324+dkalek1@users.noreply.github.com>
2022-10-22 16:37:11 +0900

    Delete src directory

--- src/Components/DayOfTheWeek.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function DayOfTheWeek() {
-  const week = ['일', '월', '화', '수', '목', '금', '토'];
-  const result = week.map((day) => <th>{day}</th>);
-
-  return <thead>{result}</thead>;
-}
--- src/Components/WeekMaker.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export default function WeekMaker({ data }) {
-  const result = data.map(item=><tb key={item.id}>{item.day}</tb>);
-  return { result };
-}
